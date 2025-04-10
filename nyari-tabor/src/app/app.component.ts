c3ad680bc778e9bbf2c8a7e49d76a551c688afad
Vizi Fanni <h379474@stud.u-szeged.hu>
2025-04-10 20:37:39 +0200

    basic html+css

--- nyari-tabor/src/app/app.component.ts
+++ nyari-tabor/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {HomeComponent} from './pages/home/home.component';
+import {ActivitiesComponent} from './pages/activities/activities.component';
+import {SignupComponent} from './pages/signup/signup.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, ActivitiesComponent, SignupComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'nyari-tabor';
+  page = "home";
 }
